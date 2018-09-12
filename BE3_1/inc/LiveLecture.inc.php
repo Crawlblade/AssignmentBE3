ea49553e46ad173fb6eb5bc4137821a211b53157
foo <foo@foo.bar>
2018-09-12 15:24:44 +0200

    added assignment BE3_1

---
+++ BE3_1/inc/LiveLecture.inc.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Description of LiveLecture
+ * 
+ * @author KJK
+ */
+class LiveLecture extends Product {
+    protected $lecturer;
+    protected $duration;
+    protected $topic;
+
+    public function getLecturer() {
+
+    }
+
+    public function getDuration() {
+
+    }
+
+    public function getTopic() {
+
+    }
+
+    public function display() {
+        printf("<p></p>")
+    }
+}
