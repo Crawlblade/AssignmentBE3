99e293d5fae05dad5d9a5cc4f1a4a0af3accc9f3
foo <foo@foo.bar>
2018-09-12 14:20:08 +0200

    Initial commit

---
+++ Car.inc.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Description of Car
+ * 
+ * @author KJK
+ */
+
+ class Car extends Vehicle {
+     protected $doors;
+     protected $trailer;
+
+     public function getTrailer(){
+
+     }
+     
+     public function getDoors(){
+
+     }
+
+     public function __toString(){
+
+     }
+
+ }
